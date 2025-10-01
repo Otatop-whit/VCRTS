07ab73fb7a07b5f9645d74b2f400bcefa7690763
Otatop-whit <bwhitley278@gmail.com>
2025-10-01 15:53:10 -0400

    Added Vehicle Inventory Manager

---
+++ src/VehicleInventoryManager.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class VehicleInventoryManager{
+    private static VehicleInventoryManager inventory_instance;
+    private HashMap<String, Vehicle> vehicleInventory;
+    private VehicleInventoryManager(){
+        vehicleInventory = new HashMap<String, Vehicle>();
+    }
+    public static VehicleInventoryManager getInstance(){
+        if(inventory_instance == null){
+            inventory_instance = new VehicleInventoryManager();
+        }
+        return inventory_instance;
+    }
+
+    public void addVehicle(String vehicleID, Vehicle vehicle){
+        vehicleInventory.put(vehicleID, vehicle);
+    }
+
+    public void removeVehicle(String vehicleID){
+        vehicleInventory.remove(vehicleID);
+    }
+}
