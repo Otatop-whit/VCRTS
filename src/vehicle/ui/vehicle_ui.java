40c8bec30a684008ae9d770e6cbcdd300ac9f66f
Xiaoyaowu21 <xiaoyao.wu21@my.stjohns.edu>
2025-10-06 18:47:01 -0400

    Update vehicle_ui.java

--- src/vehicle/ui/vehicle_ui.java
+++ src/vehicle/ui/vehicle_ui.java
@@ -14,7 +14,7 @@ public class vehicle_ui {
 
     public vehicle_ui(){
         VehicleInventoryController inventoryController =  new VehicleInventoryController();
-        JFrame window = new JFrame("Vehicle owner");
+            JFrame window = new JFrame("Vehicle owner");
       //60px from left edge of window, 100px from right, width is 500, height is 600
         window.setBounds(60,100,500,600); 
       //make window visible
@@ -31,8 +31,8 @@ public class vehicle_ui {
         account_btn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-            	//need to connect to logging page
-                System.out.println("Owner id:");
+            	//need to connect 
+                JOptionPane.showMessageDialog(null, "Owner ID: ", "Your info", JOptionPane.INFORMATION_MESSAGE);
             }
         });
     /*vehicle button: check vehicle id, model, year
@@ -45,7 +45,7 @@ public class vehicle_ui {
         vehicle_btn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-            	String[]options = {"Register Vehicle", "View Vehicle Vnfo"};
+            	String[]options = {"Register vehicle", "View vehicle info"};
             	//choose an option: register vehicle or view vehicle info, the first option needs to input info, the second option needs message ,options array and initial value is 0
             	int selectedOption = JOptionPane.showOptionDialog(null, "choose an option", null, JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
             
@@ -62,89 +62,78 @@ public class vehicle_ui {
         	 JTextField make = new JTextField();
         	 JTextField year = new JTextField();
         	 JTextField licensePlate = new JTextField();
+        	 JTextField residency = new JTextField();
         	 
         	 Object[]message = {
         			 "Model: ", model,
         			 "Manufacturer: ", make,
         			 "Manufacture Year: ", year,
         			 "LicensePlate: ",licensePlate,
+        			 "Residency: ", residency,
         	 };
-
-
-        	 int options1 = JOptionPane.showConfirmDialog(null, message, "Register New Vehicle", JOptionPane.OK_CANCEL_OPTION);
-             if(options1 == JOptionPane.OK_OPTION){
-                inventoryController.addVehicle(licensePlate,model,make,year);
-                
-             }
+        	 int registerOption = JOptionPane.showConfirmDialog(null, message, "Register New Vehicle", JOptionPane.OK_CANCEL_OPTION);
          }
          //need to connect here
         	 private void viewVehicleInfo() {
         		JOptionPane.showMessageDialog(null, null, "My Vehicle", JOptionPane.INFORMATION_MESSAGE);
-                
         	 }
          
             });
        
-        JButton residency_btn = new JButton("Residency");
-        window.add(residency_btn); 
+        JButton ts_btn = new JButton("Time Selection");
+        window.add(ts_btn); 
       
-
-        residency_btn.addActionListener(new ActionListener() {
+        ts_btn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String[] options2 = {"Arrival Time", "Departure Time"};
-                int selectedOption2 = JOptionPane.showOptionDialog(null, "choose an option", null, JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options2, options2[0]);
+            	 JTextField ArrivalTime = new JTextField(10);
+            	 JTextField DepartureTime = new JTextField(10);
+            	 JTextField licensePlate = new JTextField();
+            	 
+            	 Object[]message = {
+            			 "LicensePlate: ",licensePlate,
+            			 "ArrivalTime(YYYY-MM-DD): ", ArrivalTime,
+            			 "DepartureTime(YYYY-MM-DD): ", DepartureTime,
+            			 
+            	 };
             
-                if(selectedOption2 == 0) {
-                    ArrivalDate();
-                } else if(selectedOption2 == 1) {
-                    DepartureDate();
-                }
-            }
             
-            private void ArrivalDate() {
-                JTextField dateField = new JTextField(10);
-                dateField.setText("YYYY-MM-DD");
+            int option = JOptionPane.showConfirmDialog(null, message, 
+                    "Time Selection", JOptionPane.OK_CANCEL_OPTION);
                 
-                Object[] message = {
-                    "Enter Arrival Date (YYYY-MM-DD):", dateField
-                };
-                
-                int result1 = JOptionPane.showConfirmDialog(null, message, "Arrival Date", JOptionPane.OK_CANCEL_OPTION);
-                
-                if (result1 == JOptionPane.OK_OPTION) {
-                    String dateString = dateField.getText();
-                  
-                    if (isValidDate(dateString)) {
-                        JOptionPane.showMessageDialog(null, "Arrival Date: " + dateString);
-                    } else {
-                        JOptionPane.showMessageDialog(null, "Invalid date format! Please use YYYY-MM-DD", "Error", JOptionPane.ERROR_MESSAGE);
-                    }
-                }
-            
-                
-                
-            }
-
-            private void DepartureDate() {
-                JTextField dateField = new JTextField(10);
-                dateField.setText("YYYY-MM-DD");
-                
-                Object[] message = {
-                    "Enter Departure Date (YYYY-MM-DD):", dateField
-                };
-                
-                int result2 = JOptionPane.showConfirmDialog(null, message, "Departure Date", JOptionPane.OK_CANCEL_OPTION);
-                if (result2 == JOptionPane.OK_OPTION) {
-                    String dateString = dateField.getText();
+                if (option == JOptionPane.OK_OPTION) {
+                    String license = licensePlate.getText();
+                    String arrival = ArrivalTime.getText();
+                    String departure = DepartureTime.getText();
                     
-                    if (isValidDate(dateString)) {
-                        JOptionPane.showMessageDialog(null, "Arrival Date: " + dateString);
-                    } else {
-                        JOptionPane.showMessageDialog(null, "Invalid date format! Please use YYYY-MM-DD", "Error", JOptionPane.ERROR_MESSAGE);
+                  
+                    if (license.isEmpty()) {
+                        JOptionPane.showMessageDialog(null, 
+                            "License plate cannot be empty!", "Error", 
+                            JOptionPane.ERROR_MESSAGE);
+                        return;
                     }
+                    
+                    if (!isValidDate(arrival)) {
+                        JOptionPane.showMessageDialog(null, 
+                            "Invalid arrival date format! Please use YYYY-MM-DD", 
+                            "Error", JOptionPane.ERROR_MESSAGE);
+                        return;
+                    }
+                    
+                    if (!isValidDate(departure)) {
+                        JOptionPane.showMessageDialog(null, 
+                            "Invalid departure date format! Please use YYYY-MM-DD", 
+                            "Error", JOptionPane.ERROR_MESSAGE);
+                        return;
+                        //store to database
+                    }
+                    
+            
+                    
                 }
-            }
+        };
+    
                     private boolean isValidDate(String dateStr) {
                         try {
                             SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
@@ -155,9 +144,21 @@ public class vehicle_ui {
                             return false;
                         }
                     }
-                
+                         
+    });
+     
+        JButton residency_btn = new JButton("Residency");
+        window.add(residency_btn);   
+        
+        residency_btn.addActionListener(new ActionListener() {
+        	@Override
+        	public void actionPerformed(ActionEvent e) {
+        	JOptionPane.showMessageDialog(null, "location: ", "Residency", JOptionPane.OK_CANCEL_OPTION, null);
+        }
         });
-        window.add(panel);
+        
+        
+     window.add(panel);   
     }
-
+    
 }
