be2d83409953a7932a41c6f43173937d1959f01e
Otatop-whit <bwhitley278@gmail.com>
2025-11-18 16:59:13 -0500

    Moved Checkpoint.java to VcController folder

---
+++ src/vccontroller/service/Checkpoint.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class Checkpoint {
+    private int checkpointId;
+    private String checkpointPath;
+
+    public int getCheckpointID(){
+        return checkpointId;
+    }
+    public String getCheckpointPath() {
+        return checkpointPath;
+    }
+}
