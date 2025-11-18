08f55a55fb9debcd40e351d66a61557013b65e1b
Otatop-whit <bwhitley278@gmail.com>
2025-11-18 16:51:36 -0500

    Added new methods to Client Handler

--- src/vccontroller/service/ClientHandler.java
+++ src/vccontroller/service/ClientHandler.java
@@ -24,7 +24,7 @@ public class ClientHandler implements Runnable{
             this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             this.clientRequest = bufferedReader.readLine();
             clientHandlers.add(this);
-            broadcastMessage("Hello"+ clientRequest);
+            broadcastMessage("Hello "+ clientRequest);
 
         }catch (IOException e){
 
@@ -41,9 +41,10 @@ public class ClientHandler implements Runnable{
         while (socket.isConnected()){
             try{
                 messageFromClient = bufferedReader.readLine();
-                JobOwner job = convertToJobObj(messageFromClient);
-                System.out.println(messageFromClient);
-                jobCache.addJob(job);
+
+                //JobOwner job = convertToJobObj(messageFromClient);
+                //System.out.println(messageFromClient);
+                //jobCache.addJob(job);
                 //broadcastMessage(messageFromClient);
             } catch (IOException e) {
                 System.out.println(e);
@@ -52,6 +53,7 @@ public class ClientHandler implements Runnable{
             }
         }
     }
+    //Prints out the message
     public void broadcastMessage(String message){
         for (ClientHandler clientHandler: clientHandlers){
             try{
@@ -90,6 +92,71 @@ public class ClientHandler implements Runnable{
             System.out.println(e);
         }
     }
+
+    public void acceptJob(int idx){
+        String message = "Accepted";
+        for(ClientHandler clientHandler: clientHandlers){
+            try{
+                clientHandler.bufferedWriter.write(message);
+                clientHandler.bufferedWriter.newLine();
+                clientHandler.bufferedWriter.flush();
+                System.out.println(message);
+            }catch(IOException e){
+                System.err.println(e);
+                closeEverything(socket, bufferedReader, bufferedWriter);
+            }
+        } // End of For
+        vcController.submitJob(jobCache.getJob(idx));
+    }
+
+    public void rejectJob(int idx){
+        String message = "Rejected";
+        for(ClientHandler clientHandler : clientHandlers){
+            try{
+                clientHandler.bufferedWriter.write(message);
+                clientHandler.bufferedWriter.newLine();
+                clientHandler.bufferedWriter.flush();
+                System.out.println(message);
+            }catch(IOException e){
+                System.err.println(e);
+                closeEverything(socket, bufferedReader, bufferedWriter);
+            }
+        }
+        jobCache.removeJob(idx);
+    }
+    
+    //Splits up the client message for the client request method to work
+    public String[] splitMessage(String message){
+        String[] taskRequest = new String[2];
+        if(!message.isEmpty() && message != null){
+                    String[] parts = message.split("/");
+                    taskRequest[0] = parts[0]; // Stores the method it wants to use.
+                    StringBuilder infoBuilder = new StringBuilder();
+                    for(int i = 1; i < parts.length; i++){
+                    infoBuilder.append(parts[i]);
+                    if(i < parts.length - 1) infoBuilder.append("/");
+                }
+                    taskRequest[1] = infoBuilder.toString(); // Stores the information that it wants to use for the method.
+                    System.out.println(taskRequest[1]);
+        }
+        return taskRequest;
+    }
+    //Allows specific actions to execute depending on the task and information provided
+    public Object executeRequest(String instruction, String info){
+        switch(instruction){
+            //Placeholder code names. Feel free to change them.
+            case "V_StF":
+                saveVehicle(info);
+                return "Vehicle has been saved to File.";
+            case "J_Convert":
+                JobOwner job = convertToJobObj(info);
+                System.out.println("Job successfully added to cache.");
+                jobCache.addJob(job);
+                return "Job has been added to cache.";
+            default:
+                return "Unknown task: " + instruction;
+        }
+    }
     public JobOwner convertToJobObj(String stringJob){
         String[]parts = stringJob.split("/",4);
         if(parts.length ==4) {
@@ -101,5 +168,18 @@ public class ClientHandler implements Runnable{
         return null;
 
     }
-
+    
+    //Saves vehicle information to the vehicle repo
+    public static void saveVehicle(String vehicleInfo){
+        try{
+            BufferedWriter writer = new BufferedWriter(new FileWriter("src/vccontroller/repo/VehicleData.txt", true)); // ,true allows for adding to file without rewriting
+            writer.write(vehicleInfo);
+            writer.newLine();
+            writer.close();
+        }catch(IOException e){
+            System.err.println("Failed to write to file:" + e.getMessage());
+        }
+    }
+    
+    
 }
