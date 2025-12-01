d7f04bae5454f1bc08b4670f6dad5180d98ff848
Otatop-whit <bwhitley278@gmail.com>
2025-11-30 21:11:09 -0500

    Changed host url

--- src/vccontroller/database/Database.java
+++ src/vccontroller/database/Database.java
@@ -6,7 +6,7 @@ import job.model.JobOwner;
 public class Database {
 
 	static Connection connection = null;
-	static String url = "jdbc:mysql://localhost:3306/VC3?useTimezone=true&serverTimezone=UTC";
+	static String url = "jdbc:mysql://localhost:3306/VCRTS?useTimezone=true&serverTimezone=UTC";
 	static String username = "root";
 	static String password = "638$8(5vsug!Fqb";
 
